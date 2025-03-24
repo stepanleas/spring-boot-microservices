b08bdccd8e7e796cefb948f227ba56234e38fea5
stepanleas <s.leas@developmentaid.org>
2025-03-24 10:27:08 +0200

    Implement different configuration options inside account service.

--- accounts/src/main/java/com/microservice_app/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/microservice_app/accounts/AccountsApplication.java
@@ -1,5 +1,6 @@
 package com.microservice_app.accounts;
 
+import com.microservice_app.accounts.dto.AccountContactInfoDto;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
@@ -7,10 +8,12 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(value = {AccountContactInfoDto.class})
 @OpenAPIDefinition(
 	info = @Info(
 		title = "Accounts microservice REST API Documentation",
