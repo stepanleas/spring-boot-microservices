27350293aaf542c4ee3ba74063af6494f6678ae6
stepanleas <s.leas@developmentaid.org>
2025-05-07 22:37:53 +0300

    Update contact info dtos to allow changing of configuration data.

--- cards/src/main/java/com/microservice_app/cards/dto/CardContactInfoDto.java
+++ cards/src/main/java/com/microservice_app/cards/dto/CardContactInfoDto.java
@@ -1,10 +1,17 @@
 package com.microservice_app.cards.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "cards")
-public record CardContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+@Getter
+@Setter
+public class CardContactInfoDto {
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 }
