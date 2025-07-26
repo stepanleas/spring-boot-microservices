db71ca923674c8faaffc6531fc23fc51ea431156
stepanleas <s.leas@developmentaid.org>
2025-07-26 11:37:58 +0300

    Implement request filters for passing the correlation id to microservices.

---
+++ gatewayserver/src/main/java/com/microservice_app/gatewayserver/filters/FilterUtility.java
@@ -0,0 +1,29 @@
+package com.microservice_app.gatewayserver.filters;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import java.util.List;
+
+@Component
+public class FilterUtility {
+    public static final String CORRELATION_ID = "microservice-correlation-id";
+
+    public String getCorrelationId(HttpHeaders requestHeaders) {
+        if (requestHeaders.get(CORRELATION_ID) != null) {
+            List<String> requestHeaderList = requestHeaders.get(CORRELATION_ID);
+            return requestHeaderList.stream().findFirst().get();
+        } else {
+            return null;
+        }
+    }
+
+    public ServerWebExchange setRequestHeader(ServerWebExchange exchange, String key, String value) {
+        return exchange.mutate().request(exchange.getRequest().mutate().header(key, value).build()).build();
+    }
+
+    public ServerWebExchange setCorrelationId(ServerWebExchange exchange, String correlationId) {
+        return setRequestHeader(exchange, CORRELATION_ID, correlationId);
+    }
+}
