934569f5f5bdeba121daaca5359df924609eb692
stepanleas <s.leas@developmentaid.org>
2025-08-01 15:50:14 +0300

    Add circuit breaker support in gateway service.

--- gatewayserver/src/main/java/com/microservice_app/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/microservice_app/gatewayserver/GatewayserverApplication.java
@@ -21,7 +21,9 @@ public class GatewayserverApplication {
 			.route(p -> p
 				.path("/bank/accounts/**")
 				.filters(f -> f.rewritePath("/bank/accounts/(?<segment>.*)", "/${segment}")
-					.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+					.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+					.circuitBreaker(config -> config.setName("accountsCircuitBreaker")
+						.setFallbackUri("forward:/contactSupport")))
 				.uri("lb://ACCOUNTS")
 			)
 			.route(p -> p
---
+++ gatewayserver/src/main/java/com/microservice_app/gatewayserver/controller/FallbackController.java
@@ -0,0 +1,14 @@
+package com.microservice_app.gatewayserver.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class FallbackController {
+
+    @RequestMapping("/contactSupport")
+    public Mono<String> contactSupport() {
+        return Mono.just("An error has occurred. Please try after some time or contact support team.");
+    }
+}
